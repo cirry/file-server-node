51ec16a577dd3e23df81c39e219747c4372fbd7e
cirry <812852553@qq.com>
2024-04-08 18:31:04 +0800

    coding

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const express = require('express')
 const cors = require('cors');
-const {existsSync, stat} = require('node:fs')
-const {mkdir, readdir,} = require('node:fs/promises');
+const { existsSync, stat, } = require('node:fs')
+const { mkdir, readdir, } = require('node:fs/promises');
 const {
     join,
     basename,
-    extname
-} = require('node:path');
+    extname,
 
+} = require('node:path');
+const { log } = require('node:console');
+const { downloadFile } = require('./http/downfile')
 const app = express()
 
 app.use(cors())
 
 async function read(params) {
-    const {path, type} = params
+    const { path, type } = params
     let pathArr = path.split('/')
     if (existsSync(join(__dirname, ...pathArr))) {
         try {
-            const currentPathFiles = await readdir(join(__dirname, ...pathArr), {withFileTypes: true, recursive: false});
+            const currentPathFiles = await readdir(join(__dirname, ...pathArr), { withFileTypes: true, recursive: false });
 
             if (type === 'dir') {
                 let dirs = currentPathFiles.filter(item => !item.isFile()).map(item => ({
@@ -25,9 +27,8 @@ async function read(params) {
                     name: item.name,
                     path: path + '/' + item.name
                 }))
-                return {code: 200, data: dirs, message: 'success'}
-            }
-            if (type === 'file') {
+                return { code: 200, data: dirs, message: 'success' }
+            } else if (type === 'file') {
                 let files = currentPathFiles.filter(item => !item.isDirectory()).map(file => ({
                     ...file,
                     path: path + '/' + file.name
@@ -38,14 +39,31 @@ async function read(params) {
                 }
 
                 let fileStat = await Promise.all(filesPromise)
-                return {code: 200, data: fileStat, message: 'success'}
+                return { code: 200, data: fileStat, message: 'success' }
+            } else {
+                currentPathFiles.forEach(item => item.path = path + '/' + item.name)
+                let files = currentPathFiles.filter(file => file.isFile())
+                let filesPromise = []
+                for (let i = 0; i < files.length; i++) {
+                    filesPromise.push(getFileInfo(files[i]))
+                }
+                let fileStat = await Promise.all(filesPromise)
+                fileStat.forEach(item => item.path = path + '/' + item.name)
+                log(fileStat)
+                let otherFiles = currentPathFiles.filter(file => !file.isFile()).map(item => ({
+                    type: 'dir',
+                    name: item.name,
+                    path: path + '/' + item.name
+                }))
+
+                return { code: 200, data: otherFiles.concat(fileStat), message: 'success' }
             }
 
         } catch (err) {
-            return {code: 500, data: null, message: 'Internal Server Error'}
+            return { code: 500, data: null, message: 'Internal Server Error' }
         }
     } else {
-        return {code: 404, message: 'not found', data: null}
+        return { code: 404, message: 'not found', data: null }
     }
 }
 
@@ -55,7 +73,7 @@ getFileInfo = (file) => new Promise((resolve, reject) => {
             reject(err);
             return;
         }
-        resolve({name: file.name, ext: extname(file.path), type: 'file', mtime: stats.mtime, size: stats.size})
+        resolve({ name: file.name, ext: extname(file.path).slice(1), type: 'file', mtime: stats.mtime, size: stats.size })
     })
 },)
 
@@ -63,6 +81,12 @@ app.get('/api/path', async (req, res) => {
     console.log(req.query)
     let result = await read(req.query)
     res.status(result.code).json(result)
+})
+
+app.get('/api/downloadFile', (req, res) => {
+    console.log(req.query, '/api/downloadFile')
+
+    downloadFile(req.query.filePath, res)
 })
 
 app.listen(3006, () => {
---
+++ http/downfile.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+const downloadFile = (pathUrl, res) => {
+    const readStream = fs.createReadStream(pathUrl);
+
+    const stats = fs.statSync(pathUrl);
+
+    const filename = path.basename(pathUrl);
+
+    res.writeHead(200, {
+        'Content-Type': 'application/octet-stream', //告诉浏览器这是一个二进制文件
+        'Content-Disposition': 'attachment; filename=' + filename, //告诉浏览器这是一个需要下载的文件
+        'Content-Length': stats.size
+    });
+
+    readStream.pipe(res);
+}
+
+module.exports = {
+    downloadFile
+}
